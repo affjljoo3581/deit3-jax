ce1f79253e46808d10ee2dd5c4072882d8fb9122
affjljoo3581 <affjljoo3581@gmail.com>
2024-03-30 18:30:42 +0900

    change to install python 3.10, instead of using latest version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,9 +14,9 @@
 
 # 1. Install miniconda3 (with removing legacy).
 rm -rf ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -u
-rm Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.1.2-0-Linux-x86_64.sh
+bash Miniconda3-py310_24.1.2-0-Linux-x86_64.sh -b -u
+rm Miniconda3-py310_24.1.2-0-Linux-x86_64.sh
 
 ~/miniconda3/bin/conda init bash
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
